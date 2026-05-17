9bd3fc337e2c513275b00daabcca8300442d8305
Saptarshi Ghosh <rishi.cse.24@gmail.com>
2026-05-17 19:07:47 +0100

    adds_make_file

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ COMMIT_MSG=$1
 git switch -c dev-branch
 git add .
 git commit -m $COMMIT_MSG
-git push --set-upstream origin dev-branch
+git push 
 
 docker push rishicse24/spf-server:v_0.0.1
 docker push rishicse24/spf-client:v_0.0.1
