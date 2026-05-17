fb1f60f8fbd32c2e185b621e05f2901bded2cb23
Saptarshi Ghosh <rishi.cse.24@gmail.com>
2026-05-17 19:14:32 +0100

    adds_make_file

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ if [ $# -eq 2 ] ; then
     git switch $BRANCH
     git add .
     git commit -m $COMMIT_MSG
-    git push --set-upstream $BRANCH
+    git push
 
     docker push rishicse24/spf-server:v_0.0.1
     docker push rishicse24/spf-client:v_0.0.1
