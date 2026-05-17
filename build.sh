41ae2cd96b1dd45ceeb79add47ab66750df6d838
Saptarshi Ghosh <rishi.cse.24@gmail.com>
2026-05-17 21:58:19 +0100

    adds_k8s_apps

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64  -f app/client/Dockerfile -t spf-client:v_0.0.1 app/client 
-docker buildx build --platform linux/amd64 -f app/server/Dockerfile -t spf-server:v_0.0.1 app/server
+docker buildx build --platform linux/amd64  -f app/client/Dockerfile -t rishicse24/spf-client:v_0.0.1 app/client 
+docker buildx build --platform linux/amd64 -f app/server/Dockerfile -t rishicse24/spf-server:v_0.0.1 app/server
