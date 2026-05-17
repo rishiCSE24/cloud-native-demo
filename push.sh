b08f6e13716adc1624ab8fd4fed3c08aef07db2b
Saptarshi Ghosh <rishi.cse.24@gmail.com>
2026-05-17 19:13:51 +0100

    adds_make_file

--- push.sh
+++ push.sh
@@ -1,8 +1,17 @@
-COMMIT_MSG=$1
-git switch -c dev-branch
-git add .
-git commit -m $COMMIT_MSG
-git push 
+#! /bin/bash
+set -e 
+BRANCH=$1
+COMMIT_MSG=$2
 
-docker push rishicse24/spf-server:v_0.0.1
-docker push rishicse24/spf-client:v_0.0.1
+if [ $# -eq 2 ] ; then
+    git switch $BRANCH
+    git add .
+    git commit -m $COMMIT_MSG
+    git push --set-upstream $BRANCH
+
+    docker push rishicse24/spf-server:v_0.0.1
+    docker push rishicse24/spf-client:v_0.0.1
+else 
+    echo "ERROR: INVALID_ARG --> Usage: ./push BRANCH COMMIT_MSG"
+fi
+
